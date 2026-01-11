e70d99173d9f43d913d5eca0382e1110e9869d1e
thep1ckaxe91 <ThePickaxe91@gmail.com>
2026-01-11 12:13:15 +0700

    Single Producer Single Consumer test implementation

---
+++ hardware-sympathy/spsc_atomic.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <atomic>
+#include <thread>
+#include <optional>
+#include <assert.h>
+// headers
+template <typename T>
+class SPSC
+{
+
+    alignas(64) std::atomic<size_t> head;
+    alignas(64) std::atomic<size_t> tail;
+
+    std::vector<T> buffer;
+
+public:
+    SPSC(size_t capacity) : head(0), tail(0), buffer(capacity) {};
+
+    // this implementation doesn't utilize 1 more slot i know, but we remove the need for just a single bool which waste 7 bytes
+    bool push(const T &item)
+    {
+        if ((head + 1) % buffer.size() == tail.load(std::memory_order_acquire))
+            return false;
+        buffer[head] = item;
+        head.store((head + 1) % buffer.size(), std::memory_order_release);
+        return true;
+    }
+
+    std::optional<T> pop()
+    {
+        if (tail.load(std::memory_order_relaxed) == head.load(std::memory_order_acquire))
+            return std::nullopt;
+
+        T item = buffer[tail];
+        tail.store((tail + 1) % buffer.size(), std::memory_order_release);
+        return item;
+    }
+};
+
+int main()
+{
+    int iteration;
+    size_t q_size;
+    std::cout << "Iteration: ";
+    std::cin >> iteration;
+    std::cout << "Queue size: ";
+    std::cin >> q_size;
+    SPSC<int> spsc{q_size};
+
+    std::thread prod([&]()
+                     {
+            for(int i=0;i<iteration;i++)
+                while(!spsc.push(i)){
+                    std::this_thread::yield();
+                } });
+
+    std::thread cons([&]()
+                     {
+            for (int i = 0; i < iteration; i++)
+            {
+                auto val = spsc.pop();
+                while(!val.has_value()) {std::this_thread::yield(); val = spsc.pop();} 
+
+                std::cout << val.value() << '\n';
+            } });
+
+    prod.join();
+    cons.join();
+}
